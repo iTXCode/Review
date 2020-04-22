00eddbce51a85ab951a82167c071894140a1aa05
jack-wang128801 <1632672310@qq.com>
2020-04-22 09:52:37 +0800

    study unordered_map

---
+++ cpp_up/day14_unordered/README.md
@@ -0,0 +1,6 @@
+#### 关联式容器底层实现
+
+|底层实现方式 | 容器|
+|-|-|
+|红黑树|map,set,multi_map,multi_set|
+|Hash|unordered_map,unordered_set,unordered_multimap,unordered_multiset|
--- cpp_up/day14_unordered/test_unordered.cc
+++ cpp_up/day14_unordered/test_unordered.cc
@@ -2,30 +2,61 @@
 #include<unordered_map>
 #include<unordered_set>
 #include<set>
+#include<time.h>
+#include<vector>
 
 
 using namespace std;
 
-int main(){
+//int main(){
+//
+//  unordered_map<int,int> map;
+//  map.insert(make_pair(0,1));
+//  map.insert(make_pair(4,2));
+//  map.insert(make_pair(9,5));
+//  map.insert(make_pair(5,6));
+//  auto it=map.begin();
+//  while(it!=map.end()){
+//    cout<<it->first<<" "<<it->second<<endl;
+//    it++;
+//  }
+//  cout<<map.max_size()<<endl;
+//  return 0;
+//}
 
-  set<int> s;
-  s.insert(1);
+void Test(){
+  unordered_set<int> uset;
+  set<int> set;
+  
+  srand(time(nullptr));
 
-  unordered_map<int,int> map;
-  map.insert(make_pair(0,1));
-  map.insert(make_pair(4,2));
-  map.insert(make_pair(9,5));
-  map.insert(make_pair(5,6));
-  auto it=map.begin();
-  while(it!=map.end()){
-    cout<<it->first<<" "<<it->second<<endl;
-    it++;
+  int n;
+  cin>>n; 
+  vector<int> v;
+  for(int i=0;i<n;i++){
+    v.push_back(rand());
   }
-  cout<<map.max_size()<<endl;
-  return 0;
+
+ size_t begin1=clock();
+  for(const auto& e:v){
+    set.insert(e);
+  }
+size_t end1=clock();
+ 
+size_t begin2=clock();
+for(const auto& e:v){
+    uset.insert(e);
+  }
+size_t end2=clock();
+cout<<"set insert time:"<<end1-begin1<<endl;
+cout<<"Uset insert time:"<<end2-begin2<<endl;
 }
 
 
+int main(){
+  Test();
+  return 0;
+}
 
 //#include<iostream>
 //#include<map>
