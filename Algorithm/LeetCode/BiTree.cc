af6bdbf76ded307316b2280fbc538448992d02b9
jack-wang128801 <1632672310@qq.com>
2020-05-16 23:50:30 +0800

    公共祖先的寻找

--- Algorithm/LeetCode/BiTree.cc
+++ Algorithm/LeetCode/BiTree.cc
@@ -1,3 +1,4 @@
+//最近公共祖先,二叉搜索数版本
 class Solution {
   public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -11,6 +12,34 @@ class Solution {
 
       }
 
+      return root;
+
+    }
+
+};
+
+
+//递归版本,二叉树的版本
+class Solution {
+  public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+      if(root == nullptr || p==root || q == root){
+        return root;
+
+      }
+
+      TreeNode* left=lowestCommonAncestor(root->left,p,q);
+      TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+      if(left == nullptr){
+        return right;
+
+      }
+
+      if(right==nullptr){
+        return left;
+
+      }
       return root;
 
     }
