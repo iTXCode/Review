b7cd39a1d12491ac99f1f99910a20d10bf696340
jack-wang128801 <1632672310@qq.com>
2020-10-04 11:41:15 +0800

    update code

--- cpp_base/test/test.cc
+++ cpp_base/test/test.cc
@@ -27,27 +27,109 @@
 //}
 
 
+//#include<iostream>
+//
+//using namespace std;
+//
+//class Singleton{
+//  friend Singleton* Singleton();
+//  public:
+//    static Singleton* GetInstance(){
+//      return &_sigleton;
+//    }
+//  private:
+//    Singleton()
+//
+//    {};
+//    Singleton(const Singleton& ){};
+//    static Singleton _sigleton;
+//};
+//
+//Singleton Singleton::_sigleton;
+//
+//
+//int main(){
+//
+//  cout<<Singleton::GetInstance()<<endl;
+//
+//  cout<<Singleton::GetInstance()<<endl;
+//
+//  return 0;
+//}
+
+
 #include<iostream>
+#include<mutex>
+#include<thread>
+
 
 using namespace std;
 
 class Singleton{
   public:
     static Singleton* GetInstance(){
-      return &_sigleton;
+      if(m_Instance==nullptr){
+        //添加双检查保证效率
+        //不添加的话,第二次执行到这里就会先获取锁在进行判断
+        //添加这个可以保证只有第一次执行的时候才能获取到锁
+        _mut.lock();
+        if(m_Instance==nullptr){
+          //该层判断的目的在于保证不会再次调用构造函数
+           m_Instance=new Singleton();
+          
+        } 
+        _mut.unlock();
+      }
+
+      return m_Instance;
     }
-  private:
+
+    ~Singleton(){
+      cout<<"~Singleton()"<<endl;
+    }
+
+  
+    class ClearInstance{
+      public:
+      ~ClearInstance(){
+        if(m_Instance){
+          delete m_Instance;
+          m_Instance=nullptr;
+          cout<<"~ClearInstance()"<<endl;
+        }
+      }
+    };
+
+    static ClearInstance clear;
+   private:
     Singleton(){};
-    Singleton(const Singleton& ){};
-    static Singleton _sigleton;
+    Singleton(const Singleton& )=delete;
+    Singleton& operator =(Singleton const&)=delete;
+  private:
+    static Singleton* m_Instance;
+    static mutex _mut;
+
 };
 
-Singleton Singleton::_sigleton;
 
+mutex Singleton::_mut;
+Singleton* Singleton::m_Instance=nullptr;
+Singleton::ClearInstance clear;
+
+void Fun(){
+  for(int i=0;i<10;i++){
+    cout<<Singleton::GetInstance()<<endl;
+  }
+}
 int main(){
 
-  cout<<Singleton::GetInstance()<<endl;
+  Singleton* s=Singleton::GetInstance();
+  Singleton* s1=Singleton::GetInstance();
 
-  cout<<Singleton::GetInstance()<<endl;
+
+  thread t1(Fun);
+  thread t2(Fun);
+  t1.join();
+  t2.join();
   return 0;
 }
